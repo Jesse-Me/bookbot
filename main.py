930800d32166f3a3eddc4c047378c05a924ec480
jess <96930835+Jesse-Me@users.noreply.github.com>
2025-08-07 16:16:56 +1000

    update

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("greetings boots")
+from stats import get_word_count
+
+def get_books_text(file_path):
+    with open(file_path) as f:
+        file_contents = f.read()
+
+    return file_contents
+
+
+
+## main starts here ##
+
+book = get_books_text("books/frankenstein.txt")
+
+word_count = get_word_count(book)
+
+print(f"{word_count} words found in the document")
---
+++ stats.py
@@ -0,0 +1,5 @@
+def get_word_count(book):
+    words = book.split()
+    word_count = len(words)
+
+    return word_count
