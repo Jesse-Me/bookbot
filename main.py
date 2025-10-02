bed9a451bd0f7edd86c04a1295c267c4ab381735
jess <96930835+Jesse-Me@users.noreply.github.com>
2025-10-02 16:34:30 +1000

    update

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_word_count
+from stats import get_word_count, get_char_count, get_sorted_char_count
 
 def get_books_text(file_path):
     with open(file_path) as f:
@@ -15,3 +15,8 @@ book = get_books_text("books/frankenstein.txt")
 word_count = get_word_count(book)
 
 print(f"{word_count} words found in the document")
+
+char_count = get_char_count(book) 
+
+sorted_char_count = get_sorted_char_count(char_count)
+
--- stats.py
+++ stats.py
@@ -3,3 +3,30 @@ def get_word_count(book):
     word_count = len(words)
 
     return word_count
+
+def get_char_count(file_contents): 
+    
+    results = {} 
+    file_contents = file_contents.lower()
+    
+    for character in file_contents:
+       results[character] = results.get(character, 0) + 1
+    
+    return results
+ 
+def get_sorted_char_count(char_count):
+    
+    sorted_list = []
+    char_count = char_count.isalpha()
+    for key, value in char_count.items():
+        
+        sorted_list.append({"char":key, "num": value})
+        
+    
+    
+
+    for item in sorted_list:
+        print(f"{item['char']}: {item['num']}")
+    
+    return sorted_list
+    
